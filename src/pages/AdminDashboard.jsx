fb68573a0b8a48342355be353783da0758497b84
Bessmack <jenwied4lif@gmail.com>
2025-10-18 15:54:49 +0300

    making a Stats.jsx and updating App.css

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import Stats from "../components/Stats";
 
 function AdminDashboard(){
     return(
         <>
         <Navbar/>
+        <Stats/>
         </>
     )
 }
